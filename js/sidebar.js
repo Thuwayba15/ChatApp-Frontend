f8d0f75df3d8c57175574b1bb5c59be143021496
Thuwayba15 <thuwaybadawood15@gmail.com>
2026-02-12 08:34:37 +0200

    Add real-time direct messages

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,21 @@
-//Show list of online users
-
 const USERS_KEY = "chat-o.users";
 const CURRENT_USER_KEY = "chat-o.current-user";
 
+const CHATS_KEY = "chat-o.chats";
+const MESSAGES_KEY = "chat-o.messages";
+
+const NEXT_CHAT_ID_KEY = "chat-o.nextChatId";
+const NEXT_MESSAGE_ID_KEY = "chat-o.nextMessageId";
+
+//Listing in sidebar
 const chatList = document.getElementById('chatList');
 const empty = document.getElementById('chatListEmpty');
 const tabs = document.getElementById('tabs');
+
+//Chats themselves
+const messages = document.getElementById('messages');
+const msgInput = document.getElementById('msg-input');
+const sendBtn = document.getElementById('send-btn');
 
 //make sure only logged in users can see index
 const current = sessionStorage.getItem(CURRENT_USER_KEY);
@@ -15,6 +25,8 @@ if(!current){
 
 //Set default tab to be selected
 let activeTab = 'all';
+//Keep track of chat that is currently open
+let activeChatId = null;
 
 function loadUsers() {
   const raw = localStorage.getItem(USERS_KEY);
@@ -30,6 +42,89 @@ function getCurrentUserId() {
   const raw = sessionStorage.getItem(CURRENT_USER_KEY);
   if (!raw) return null;
   return Number(raw);
+}
+
+//Chat functions
+function loadChats() {
+    const raw = localStorage.getItem(CHATS_KEY);
+    if (!raw) return [];
+    return JSON.parse(raw);
+}
+
+function saveChats(chats) {
+    localStorage.setItem(CHATS_KEY, JSON.stringify(chats));
+}
+
+function getNextChatId() {
+    const raw = localStorage.getItem(NEXT_CHAT_ID_KEY);
+    const current = raw ? Number(raw) : 1;
+    localStorage.setItem(NEXT_CHAT_ID_KEY, String(current + 1));
+    return current;
+}
+
+function getOrCreateChat(me, you){
+    const chats = loadChats();
+
+    const existing = chats.find((c) => 
+        c.type === 'direct' &&
+        c.members.length === 2 &&
+        c.members.includes(me) &&
+        c.members.includes(you)
+    );
+
+    if(existing) return existing;
+
+    const chat = {
+        id: getNextChatId(),
+        type: 'direct',
+        members: [me, you],
+        timeStamp: Date.now(),
+    }
+
+    chats.push(chat);
+    saveChats(chats);
+    return chat;
+}
+
+//Message functions
+
+function loadMessages() {
+    const raw = localStorage.getItem(MESSAGES_KEY);
+    if (!raw) return [];
+    return JSON.parse(raw);
+}
+
+function saveMessages(messages) {
+    localStorage.setItem(MESSAGES_KEY, JSON.stringify(messages));
+}
+
+function getNextMessageId() {
+    const raw = localStorage.getItem(NEXT_MESSAGE_ID_KEY);
+    const current = raw ? Number(raw) : 1;
+    localStorage.setItem(NEXT_MESSAGE_ID_KEY, String(current + 1));
+    return current;
+}
+
+function sendMessage(chatId, senderId, content) {
+    const messages = loadMessages();
+    const chats = loadChats();
+
+    const message = {
+        id: getNextMessageId(),
+        chatId,
+        senderId,
+        content,
+        timeStamp: Date.now(),
+    }
+
+    messages.push(message);
+    saveMessages(messages);
+
+    const chat = chats.find((c) => c.id === chatId);
+    if(chat){
+        chat.timeStamp = message.timeStamp;
+        saveChats(chats);
+    }
 }
 
 function getVisibleUsers(users) {
@@ -52,51 +147,191 @@ function getVisibleUsers(users) {
     return notMe;
 }
 
-function render() {
-    const users = loadUsers();
-    const visible = getVisibleUsers(users);
+function getAllChatsVisibleUsers(){
+    const me = getCurrentUserId();
+    if(me === null) return [];
 
-    //clear items before rendering
-    chatList.innerHTML = '';
+    const chats = loadChats()
+     .filter((c) => c.type === 'direct' && c.members.includes(me))
+     .sort((a,b) => b.timeStamp - a.timeStamp);
 
-    //if it's empty, show empty list, otherwise shpw users
-    if(visible.length === 0){
-        empty.hidden = false;
+    const users = chats.map((c) => c.members.find((id) => id !== me));
+    return users.map((id) => loadUsers().find((u) => u.id === id)).filter((u) => u !== undefined);
+}
+
+function getOnlineVisibleUsers() {
+    const me = getCurrentUserId();
+
+    return loadUsers()
+        .filter((u) => u.id !== me && u.isOnline === true);
+}
+
+function getAllChatsSidebarItems(){
+    const me = getCurrentUserId();
+    if(me === null) return [];
+
+    return loadChats().filter((c) => c.members.includes(me))
+    .sort((a,b) => b.timeStamp - a.timeStamp)
+    .map((c) => {
+        if(c.type === 'direct') {
+            const otherUserId = c.members.find((id) => id !== me);
+            const otherUser = loadUsers().find((u) => u.id === otherUserId);
+            return {
+                type: 'direct',
+                chatId: c.id,
+                user: otherUser
+            };
+        }
+
+        return {
+            type: 'group',
+            chatId: c.id,
+            title: c.title,
+        }
+    })
+}
+function renderMessages() {
+    const me = getCurrentUserId();
+    messages.innerHTML = '';
+
+    if(activeChatId === null) {
         return;
     }
 
-    //Assuming it's not empty
-    empty.hidden = true;
+    const allMessages = loadMessages().filter((m) => m.chatId === activeChatId).sort((a,b) => a.timeStamp - b.timeStamp);
 
-    for (const user of visible){
-        const button = document.createElement('button');
-        button.type = 'button';
-        button.className = 'chat-list-item';
+    const chats = loadChats().find((c) => c.id === activeChatId);
 
-        //IMAGE PLACEHOLDER
-        const pic = document.createElement('div');
-        pic.className = 'chat-list-item-pic';
+    const isGroup = chats?.type === 'group';
 
-        const info = document.createElement('div');
-        info.className = 'chat-list-item-info';
+    for (const message of allMessages){
+        const msgDiv = document.createElement('div');
+        msgDiv.className = message.senderId === me ? 'msg-out' : 'msg-in';
 
-        const name = document.createElement('span');
-        name.className = 'chat-list-item-name';
-        name.textContent = user.username;
+        if(isGroup && message.senderId !== me){
+            const sender = document.createElement('div');
+            sender.className = 'msg-sender';
+            sender.textContent = loadUsers().find((u) => u.id === message.senderId)?.username || 'Unknown';
+            msgDiv.appendChild(sender);
+        }
 
-        const badge = document.createElement('span');
-        badge.className = 'chat-list-item-badge';
-        badge.textContent = user.isOnline ? 'Online' : 'Offline';
+        const content = document.createElement('div');
+        content.className = 'msg-content';
+        content.textContent = message.content;
 
-        info.appendChild(name);
-        info.appendChild(badge);
+        const time = document.createElement('div');
+        time.className = 'msg-time';
+        time.textContent = new Date(message.timeStamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 
-        button.appendChild(pic);
-        button.appendChild(info);
+        msgDiv.appendChild(content);
+        msgDiv.appendChild(time);
 
-        chatList.appendChild(button);
+        messages.appendChild(msgDiv);
     }
+
+    messages.scrollTop = messages.scrollHeight;
 }
+
+function render() {
+  chatList.innerHTML = "";
+
+  let items = [];
+
+  if (activeTab === 'online') {
+    // online tab shows USERS (not chats)
+    items = getOnlineVisibleUsers().map((u) => ({
+      type: 'userPick',
+      user: u,
+    }));
+  } else if (activeTab === 'all') {
+    // all tab shows DIRECT + GROUP chats
+    items = getAllChatsSidebarItems();
+  } else if (activeTab === 'groups') {
+    // groups tab shows only group chats 
+    items = getAllChatsSidebarItems().filter((i) => i.type === "group");
+  }
+
+  if (items.length === 0) {
+    empty.hidden = false;
+    return;
+  }
+
+  empty.hidden = true;
+
+  for (const item of items) {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'chat-list-item';
+
+    const pic = document.createElement('div');
+    pic.className = 'chat-list-item-pic';
+
+    const info = document.createElement('div');
+    info.className = 'chat-list-item-info';
+
+    const name = document.createElement('span');
+    name.className = 'chat-list-item-name';
+
+    const badge = document.createElement('span');
+    badge.className = 'chat-list-item-badge';
+
+    if (item.type === 'direct') {
+      name.textContent = item.user.username;
+
+      if (item.user.isOnline) {
+        badge.textContent = 'Online';
+        badge.classList.add('badge-online');
+      } else {
+        badge.textContent = 'Offline';
+        badge.classList.add('badge-offline'); 
+      }
+
+      button.addEventListener('click', () => {
+        activeChatId = item.chatId;
+        renderMessages();
+      });
+    }
+
+    if (item.type === 'group') {
+      name.textContent = item.title;
+      badge.textContent = 'Group';
+
+      button.addEventListener('click', () => {
+        activeChatId = item.chatId;
+        renderMessages();
+      });
+    }
+
+    // Online tab: clicking a user should create/open direct chat
+    if (item.type === 'userPick') {
+      const user = item.user;
+      name.textContent = user.username;
+
+      badge.textContent = 'Online';
+      badge.classList.add('badge--online');
+
+      button.addEventListener('click', () => {
+        const me = getCurrentUserId();
+        if (me === null) return;
+
+        const chat = getOrCreateChat(me, user.id);
+        activeChatId = chat.id;
+
+        renderMessages();
+        render(); // refresh sidebar ordering
+      });
+    }
+
+    info.appendChild(name);
+    info.appendChild(badge);
+
+    button.appendChild(pic);
+    button.appendChild(info);
+
+    chatList.appendChild(button);
+  }
+}
+
 
 tabs.addEventListener('click', (event) => {
     const clicked = event.target.closest('button');
@@ -117,12 +352,36 @@ tabs.addEventListener('click', (event) => {
     render();
 });
 
+sendBtn.addEventListener('click', () => {
+    const me = getCurrentUserId();
+    if(me === null) return;
+
+    const content = msgInput.value.trim();
+
+    if(activeChatId === null){
+        alert('No chat selected');
+        return;
+    }
+
+    sendMessage(activeChatId, me, content);
+    msgInput.value = '';
+    renderMessages();
+    render();
+});
+
 //Check that it updates when users go offline
 window.addEventListener('storage', (event) => {
-  if (event.key === USERS_KEY || event.key === CURRENT_USER_KEY) {
+  if (event.key === USERS_KEY || event.key === CHATS_KEY|| event.key === MESSAGES_KEY) {
     render();
+    renderMessages();
   }
 });
 
+function init(){
+    const defalt = document.getElementById('active');
+    
+    render();
+    renderMessages();
+}
 
-render();
+init();
