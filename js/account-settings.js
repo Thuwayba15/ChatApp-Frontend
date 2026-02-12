5ef4b36530bf5ce5176d3f546c3f4c6f27c1bcaa
Thuwayba15 <thuwaybadawood15@gmail.com>
2026-02-12 12:52:31 +0200

    Edit account UI to match wireframes

--- js/account-settings.js
+++ js/account-settings.js
@@ -3,6 +3,23 @@
 const CURRENT_USER_KEY = 'chat-o.current-user';
 const USERS_KEY = 'chat-o.users';
 
+const cancelButton = document.getElementById('cancel-button');
+const userName = document.getElementById('userName');
+
+function renderCurrentUserName() {
+  const me = getCurrentUserId();
+  if (me === null) return;
+
+  const users = loadUsers();
+  const user = users.find((u) => u.id === me);
+
+  if (!user) return;
+
+  userName.textContent = user.username; 
+}
+
+renderCurrentUserName();
+
 const current = sessionStorage.getItem(CURRENT_USER_KEY);
 if(!current){
     window.location.href = '../pages/login.html';
@@ -40,3 +57,9 @@ logoutButton.addEventListener('click', () => {
     sessionStorage.removeItem(CURRENT_USER_KEY);
     window.location.href = '../pages/login.html';
 })
+
+
+
+cancelButton.addEventListener('click', () => {
+  window.location.href = '../index.html';
+});
